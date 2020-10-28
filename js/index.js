f1141fda0b520d57b831cca7fbc4443550b947f1
BraydenB-13 <brayden.barger8273@stu.mtec.edu>
2020-10-28 09:50:34 -0600

    js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function takeInfo() {
+    let infoArr = [document.getElementById('course'), document.getElementById('tee'), document.getElementById('players')];
+    let info = ("Course: " + infoArr[0].value + ", " + "Tee: " + infoArr[1].value + ", " + "Players: "  + infoArr[2].value + ".");
+    console.log(info);
+    location.href = "./scorecard.html";
+    printInfo();
+};
+
+function printInfo() {
+    document.getElementById('info').innerHTML = info;
+}
